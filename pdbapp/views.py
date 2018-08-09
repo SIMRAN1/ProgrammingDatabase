71650c1c2a22ccb8323f10699f307843328cfa3f
SIMRAN1 <simranpreetkaurkhalsa13@gmail.com>
2018-08-09 11:40:33 +0530

    Loding Models into Templates

--- pdbapp/views.py
+++ pdbapp/views.py
@@ -4,10 +4,12 @@ from django.shortcuts import render
 # Create your views here.
 from django.template import loader
 
+from pdbapp.models import Item
+
 
 def index(request):
     template = loader.get_template('index.html')
     context = {
-        'message' : 'Hello world'
+        'java' : Item.objects.get(id='1')
     }
     return HttpResponse(template.render(context,request))
