3306b287ef3c5cd203fb79150005eb96e96401cf
SIMRAN1 <simranpreetkaurkhalsa13@gmail.com>
2018-08-09 10:52:15 +0530

    Adding Basic tempalte

--- pdbapp/views.py
+++ pdbapp/views.py
@@ -2,5 +2,12 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.template import loader
+
+
 def index(request):
-    return HttpResponse('<h1> Programming Database</h1>')
+    template = loader.get_template('index.html')
+    context = {
+
+    }
+    return HttpResponse(template.render(context,request))
