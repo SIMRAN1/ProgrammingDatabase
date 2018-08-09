b808028d4aab212bbb2283f6035b86c952023c2b
SIMRAN1 <simranpreetkaurkhalsa13@gmail.com>
2018-08-09 17:29:15 +0530

    Adding Category Model

--- pdbapp/admin.py
+++ pdbapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from pdbapp.models import Item
+from pdbapp.models import Item,Category
 
+admin.site.register(Category)
 admin.site.register(Item)
--- pdbapp/models.py
+++ pdbapp/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    class Meta:
+        verbose_name_plural = 'Categories'
+    name = models.CharField(max_length=32)
+
+    def  __str__(self):
+        return self.name
+
 class Item(models.Model):
     name = models.CharField(max_length=32)
     description = models.TextField()
