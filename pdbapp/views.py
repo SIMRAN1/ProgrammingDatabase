d33568619971563c27552565422d9e557199b32c
SIMRAN1 <simranpreetkaurkhalsa13@gmail.com>
2018-08-09 10:53:45 +0530

    Passing values from context to template

--- pdbapp/views.py
+++ pdbapp/views.py
@@ -8,6 +8,6 @@ from django.template import loader
 def index(request):
     template = loader.get_template('index.html')
     context = {
-
+        'message' : 'Hello world'
     }
     return HttpResponse(template.render(context,request))
