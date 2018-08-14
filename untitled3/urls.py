a3fe158078f51f67aeefc661ba1e66609c56444c
SIMRAN1 <simranpreetkaurkhalsa13@gmail.com>
2018-08-14 23:02:43 +0530

    Loading of images completed

--- untitled3/urls.py
+++ untitled3/urls.py
@@ -13,10 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
+from untitled3 import settings
 
 urlpatterns = [
     path('',include('pdbapp.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
